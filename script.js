b86a208eecbbf4cff2c4d7e6f1d8d600164472e6
alfredoascen <alfredoascen1@gmail.com>
2024-08-08 09:26:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Reemplaza 'VIDEO_ID' con el ID de tu video de YouTube
+const videoId = 'VIDEO_ID';
+const youtubeUrl = `https://www.youtube.com/embed/${videoId}`;
+
+document.getElementById('youtube-video').src = youtubeUrl;
