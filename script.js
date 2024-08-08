0041cdb089423cf068e306e8a547635ebf0d3ea7
alfredoascen <alfredoascen1@gmail.com>
2024-08-08 22:06:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // script.js
 
 // Reemplaza 'VIDEO_ID' con el ID de tu video de YouTube
-const videoId = 'VIDEO_ID';
+const videoId = 'sqNqeQpP83Y?si=2a1fuX1sR8q6WzPI';
 const youtubeUrl = `https://www.youtube.com/embed/${videoId}`;
 
 document.getElementById('youtube-video').src = youtubeUrl;
